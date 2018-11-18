c1796c2da4ce13b360581961b677955136679738
David Câmara <davidcamara21@hotmail.com>
2018-11-18 12:38:02 +0000

    Update azureManipulate_SGF.php

--- database/azureManipulate_SGF.php
+++ database/azureManipulate_SGF.php
@@ -5,7 +5,7 @@ $possibleTypes = array("image/jpeg", "image/png", "audio/mp3", "video/mp4");
 //storage azure server
 $server = "https://tunafiles.file.core.windows.net/tunafiles/";
 $credential = "?sv=2018-03-28&si=tunafiles&sr=s&sig=778ot79hDkKKCDMKcPDOpXDPUud%2FOzCHEO2Hj%2B0G41Y%3D";
-$isLocalhost = TRUE;
+$isLocalhost = FALSE;
 
 if ($isLocalhost) {
     $linkRoot = "$_SERVER[HTTP_HOST]/tuna/SMI_site";
